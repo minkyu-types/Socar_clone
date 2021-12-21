09a04c2cdafa57bbf8d8fb93a87a54bb15be20f4
its-mingyu <lion9638@naver.com>
2021-12-21 21:46:58 +0900

    [2021 - 12 - 17 ( 금 )] [ 1 차 ] 1. 본인인증 약관동의, 레이아웃 상대성 조절
    [ What was the problem? ]
    1. 본인인증 페이지 마무리하기
    
    [Have-to-do]
    1. 휴대폰 본인인증 어떻게 하는지
    2. 콤보박스 누르면 아래에서 팝업 올라오게 하는거 어떻게 하는지
    3. 액티비티 이동간에 anim 넣는 방법

--- app/src/main/java/com/studying/socarclone/sign/AuthenticationActivity.java
+++ app/src/main/java/com/studying/socarclone/sign/AuthenticationActivity.java
@@ -11,12 +11,17 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 import com.studying.socarclone.R;
 import com.studying.socarclone.databinding.ActivityAuthenticationBinding;
 
 public class AuthenticationActivity extends AppCompatActivity {
     ActivityAuthenticationBinding authenticationBinding;
+    int terms_authenticate_1;
+    int terms_authenticate_1_1, terms_authenticate_1_2, terms_authenticate_1_3, terms_authenticate_1_4, terms_authenticate_1_5;
+    int name_check, jumin_check, phonenum_check;
+    int phone_authenticate;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,27 +37,74 @@ public class AuthenticationActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        authenticationBinding.imageviewAuthGo.setOnClickListener(view -> {
+            if(authenticationBinding.constraintlayoutAuth1.getVisibility() == View.GONE){
+                authenticationBinding.constraintlayoutAuth1.setVisibility(View.VISIBLE);
+            } else {
+                authenticationBinding.constraintlayoutAuth1.setVisibility(View.GONE);
+            }
+        });
+
+        authenticationBinding.authenticationCheckbox1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(authenticationBinding.authCheckbox1.isChecked()){
+                    terms_authenticate_1 = 1;
+                } else {
+                    terms_authenticate_1 = 0;
+                }
+            }
+        });
+
         authenticationBinding.linearlayoutAuth1.setOnClickListener(view -> {
             if(authenticationBinding.authCheckbox1.isChecked()){
-
+                authenticationBinding.authenticationCheckbox1.setEnabled(true);
+                authenticationBinding.authenticationCheckbox2.setEnabled(true);
+                authenticationBinding.authenticationCheckbox3.setEnabled(true);
+                authenticationBinding.authenticationCheckbox4.setEnabled(true);
+                authenticationBinding.authenticationCheckbox5.setEnabled(true);
             } else {
-
+                authenticationBinding.authenticationCheckbox1.setEnabled(false);
+                authenticationBinding.authenticationCheckbox2.setEnabled(false);
+                authenticationBinding.authenticationCheckbox3.setEnabled(false);
+                authenticationBinding.authenticationCheckbox4.setEnabled(false);
+                authenticationBinding.authenticationCheckbox5.setEnabled(false);
             }
         });
         authenticationBinding.relativelayoutAuthenticate1.setOnClickListener(view -> {
-
+            if(authenticationBinding.authenticationCheckbox1.isChecked()){
+                terms_authenticate_1_1 = 1;
+            } else {
+                terms_authenticate_1_1 = 0;
+            }
         });
         authenticationBinding.relativelayoutAuthenticate2.setOnClickListener(view -> {
-
+            if(authenticationBinding.authenticationCheckbox1.isChecked()){
+                terms_authenticate_1_2 = 1;
+            } else {
+                terms_authenticate_1_2 = 0;
+            }
         });
         authenticationBinding.relativelayoutAuthenticate3.setOnClickListener(view -> {
-
+            if(authenticationBinding.authenticationCheckbox1.isChecked()){
+                terms_authenticate_1_3 = 1;
+            } else {
+                terms_authenticate_1_3 = 0;
+            }
         });
         authenticationBinding.relativelayoutAuthenticate4.setOnClickListener(view -> {
-
+            if(authenticationBinding.authenticationCheckbox1.isChecked()){
+                terms_authenticate_1_4 = 1;
+            } else {
+                terms_authenticate_1_4 = 0;
+            }
         });
         authenticationBinding.relativelayoutAuthenticate5.setOnClickListener(view -> {
-
+            if(authenticationBinding.authenticationCheckbox1.isChecked()){
+                terms_authenticate_1_5 = 1;
+            } else {
+                terms_authenticate_1_5 = 0;
+            }
         });
     }
 
